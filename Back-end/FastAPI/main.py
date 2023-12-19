7c90d5577eafecff9a55b9c6e72b0bd62a3cdf12
= <jblenis98@gmail.com>
2023-12-19 16:31:49 -0300

    primeros pasos con FastAPI

---
+++ Back-end/FastAPI/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app= FastAPI()
+
+@app.get("/")
+async def root():
+    return "Hola FastAPI!"
+
+@app.get("/url")
+async def url():
+    return {"url": "https://google.com"}
