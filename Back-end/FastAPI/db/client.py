a63b72bdfc23b6ce507e9f98d22f1d1fd1df3e13
= <jblenis98@gmail.com>
2024-02-22 17:24:56 -0300

    finish

--- Back-end/FastAPI/db/client.py
+++ Back-end/FastAPI/db/client.py
@@ -1,3 +1,9 @@
 from pymongo import MongoClient
 
-db_client= MongoClient()
+
+#Base de datos local
+#db_client= MongoClient().local
+
+
+#Base de datos remota
+db_client= MongoClient('mongodb+srv://test:test1@cluster0.d3zx3lu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').test
